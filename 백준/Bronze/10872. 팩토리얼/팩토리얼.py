affbf1fb192470863d1aac13681af80264d687e4
La_Ska <129167542+flaska99@users.noreply.github.com>
2025-03-17 14:35:26 +0900

    [Bronze III] Title: 팩토리얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.py
@@ -1,5 +1,15 @@
-n = int(input())
-pac = 1
-for i in range(1, n+1) : pac *= i
+# n = int(input())
+# pac = 1
+# for i in range(1, n+1) : pac *= i
 
-print('1' if n == 0 else pac)
+# print('1' if n == 0 else pac)
+
+# 재귀로 구현
+def fac(n) :
+  if n <= 1 :
+    return 1
+  else :
+    return n * fac(n-1)
+
+a = int(input())
+print(fac(a))
