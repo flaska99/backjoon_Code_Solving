2022dc3454e4da13fe669534cd0c9de88c9a6686
La_Ska <129167542+flaska99@users.noreply.github.com>
2025-04-08 20:36:17 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,17 +1,28 @@
+import sys
 
-
-def dfs(i, total):
-    if total > i :
-        return 0
-    
-    if total == i :
-        return 1
-    
-    return dfs(i, total + 1) + dfs(i, total + 2) + dfs(i, total + 3)
-
+input = sys.stdin.readline
 
 t = int(input())
 
-for i in range(t) :
+for _ in range(t):
     n = int(input())
-    print(dfs(n, 0))
+
+    if n == 1:
+        print(1)
+
+    elif n == 2:
+        print(2)
+    elif n == 3:
+        print(4)
+    else:
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 4
+
+        for i in range(4, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+        print(dp[n])
+
+
